cb2af192784615c2eacd59a5c59bd03b731f35bf
Soulou <leo.unbek@gmail.com>
2013-05-15 17:20:20 +0200

    Restart and not reload server avec writing config files

--- recipes/server.rb
+++ recipes/server.rb
@@ -78,7 +78,7 @@ template "#{node['postgresql']['dir']}/pg_hba.conf" do
   owner "postgres"
   group "postgres"
   mode 00600
-  notifies :reload, 'service[postgresql]', :immediately
+  notifies :restart, 'service[postgresql]', :immediately
 end
 
 # NOTE: Consider two facts before modifying "assign-postgres-password":
